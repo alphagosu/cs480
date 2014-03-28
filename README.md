a81a9d8e84b1101c34fadf6299e9bd05c612b2b7
jrile <jrile@jrile-VirtualBox.(none)>
2014-03-28 11:11:27 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+ == CS480 research collaboration tool
+ruby on rails 4.0.4
+ruby 2.1.0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rct::Application.config.secret_key_base = 'f943cc808dd59d1316ac24abe808361c642cd006bf9f394c8e3b02890069cbe2ffd863e3e0270c749315c1e212885cf2cda0426737b66b794e3ffade62ebd634'

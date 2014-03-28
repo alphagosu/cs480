bdaaadb42183cbeb8b045c5c61858dff59757817
jrile <jrile@jrile-VirtualBox.(none)>
2014-03-28 14:23:19 -0400

    Registration routing done. Header complete for signed-out visitors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rct::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   resources :collaborations
 
   resources :users
+
+  root 'static_pages#home'
+  match '/registration', to: 'users#new', via: 'get'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

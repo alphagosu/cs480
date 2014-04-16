fe5bd2a22bb2cfa558901727f8c2ec3cf8cfc120
Jonathan Rile <jonathanrile@gmail.com>
2014-04-15 20:08:48 -0400

    Holy crap messaging works. But it looks like poop.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
   before_filter :get_mailbox
   before_filter :get_box
+  before_filter :get_user
   before_filter :check_current_subject_in_conversation, :only => [:show, :update, :destroy]
   def index
     @conversations = @mailbox.inbox.page(params[:page])
@@ -9,6 +10,26 @@ class ConversationsController < ApplicationController
 
   def show
     @receipts = @mailbox.receipts_for(@conversation).not_trash
+  end
+
+  def update
+    if params[:untrash].present?
+      @conversation.untrash(@user)
+    end
+
+    if params[:reply_all].present?
+      last_receipt = @mailbox.receipts_for(@conversation).last
+      @receipt = @user.reply_to_all(last_receipt, params[:body])
+    end
+
+    if @box.eql? 'trash'
+      @receipts = @mailbox.receipts_for(@conversation).trash
+    else
+      @receipts = @mailbox.receipts_for(@conversation).not_trash
+    end
+
+    redirect_to action: :show
+    @receipts.mark_as_read
   end
 
   private
@@ -24,6 +45,10 @@ class ConversationsController < ApplicationController
       redirect_to conversations_path(:box => @box)
       return
     end
+  end
+
+  def get_user
+    @user = current_user
   end
 
   def get_box
